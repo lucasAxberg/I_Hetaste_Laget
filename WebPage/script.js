4cf76136c8d4622a906c60e6801e9be1d791fd20
Lucas Axberg <lucas.axberg@abbgymnasiet.se>
2022-10-18 13:06:41 +0200

    Fixed farenheit/celsius convertion

--- WebPage/script.js
+++ WebPage/script.js
@@ -1,4 +1,4 @@
-function changeActive(obj) {
+function changeActive1(obj) {
 	// alert(obj.id);
 	console.log(obj.id);
 
@@ -7,6 +7,37 @@ function changeActive(obj) {
 	const child = parent.children;
 	for (var i = 0; i < child.length; i++) {
 		if (child[i].classList.contains("active")) {
+			if (child[i] == obj) {
+				return;
+			}
+			child[i].classList.remove("active");
+		}
+	}
+	obj.classList.add("active");
+	Convertion(obj);
+}
+
+function Convertion(obj) {
+	let temperature = document.getElementById("temp");
+	if (obj.id == "C") {
+		temperature.innerHTML = temperature.innerHTML * 1.8 + 32;
+	} else if (obj.id == "F") {
+		temperature.innerHTML = (temperature.innerHTML - 32) / 1.8;
+	}
+}
+
+function changeActive2(obj) {
+	// alert(obj.id);
+	console.log(obj.id);
+
+	const parent = obj.parentElement;
+	console.log(parent);
+	const child = parent.children;
+	for (var i = 0; i < child.length; i++) {
+		if (child[i].classList.contains("active")) {
+			if (child[i] == obj) {
+				return;
+			}
 			child[i].classList.remove("active");
 		}
 	}
