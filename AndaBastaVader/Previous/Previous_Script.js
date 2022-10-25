51d13b584df7d6c2af18fcaeb750ea706ab1b41a
Lucas Axberg <lucas.axberg@abbgymnasiet.se>
2022-10-25 09:37:43 +0200

    Fixed the sidebar

---
+++ AndaBastaVader/Previous/Previous_Script.js
@@ -0,0 +1,6 @@
+function openMenu() {
+	document.getElementById("sideMenu").classList.toggle("toTheSide");
+	document.getElementById("stripe1").classList.toggle("change-stripe1");
+	document.getElementById("stripe2").classList.toggle("change-stripe2");
+	document.getElementById("stripe3").classList.toggle("change-stripe3");
+}
