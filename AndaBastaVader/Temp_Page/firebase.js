dcb6d974be9de85a8c732c9e7afa360b3cabfa7a
Lucas Axberg <lucas.axberg@abbgymnasiet.se>
2022-10-27 10:22:34 +0200

    fixed the link to firebase

---
+++ AndaBastaVader/Temp_Page/firebase.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBWy9aiEcXbiuDK3gOnQ_4VWkc84OpoJXk",
+
+	authDomain: "i-hetaste-lagret.firebaseapp.com",
+
+	databaseURL:
+		"https://i-hetaste-lagret-default-rtdb.europe-west1.firebasedatabase.app",
+
+	projectId: "i-hetaste-lagret",
+
+	storageBucket: "i-hetaste-lagret.appspot.com",
+
+	messagingSenderId: "608237085804",
+
+	appId: "1:608237085804:web:216a3403321d1c36db6449",
+};
+
+firebase.initializeApp(firebaseConfig);
+const dataBase = firebase.database().ref();
